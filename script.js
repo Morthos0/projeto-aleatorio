e58f33beb67ca92c928fd3bceec9c8cd46d67f4b
Morthos0 <vinicios.alves.oliveira@escola.pr.gov.br>
2025-10-16 11:25:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-function tocar(id) {
-  // Pega todos os elementos <audio>
+function tocarSom(id) {
   const audios = document.querySelectorAll('audio');
-
-  // Pausa todos os áudios
   audios.forEach(audio => {
     audio.pause();
     audio.currentTime = 0;
   });
-
-  // Toca o áudio selecionado
+  
   const audioSelecionado = document.getElementById(id);
-  if (audioSelecionado) {
-    audioSelecionado.play();
-  } else {
-    console.error('Áudio não encontrado com o id:', id);
-  }
+  audioSelecionado.play();
 }
